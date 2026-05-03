f21261f56db1ea8e7db5eb018ec83e93875457ca
Abhishek Kumar <abhishek841101@gmail.com>
2026-05-03 23:00:32 +0530

    correct  api

--- src/features/property/propertySlice.js
+++ src/features/property/propertySlice.js
@@ -6,7 +6,7 @@ import axios from "axios";
 
 // const API = "http://localhost:5000/api/properties";
 
-const API = `${import.meta.env.VITE_API_URL}/api/properties`;
+// const API = `${import.meta.env.VITE_API_URL}/api/properties`;
 
 
 
