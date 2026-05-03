a4a2c2712950543e72d7a1491c65925157d6c298
Abhishek Kumar <abhishek841101@gmail.com>
2026-05-03 11:11:10 +0530

    fix apia

--- src/features/property/propertySlice.js
+++ src/features/property/propertySlice.js
@@ -5,7 +5,7 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 // const API = "http://localhost:5000/api/properties";
-const API = `${import.meta.env.REACT_APP_API_URL}/api/properties`;
+const API = `${import.meta.env.VITE_API_URL}/api/properties`;
 // // =======================
 // AUTH HEADER
 // =======================
